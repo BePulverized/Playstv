30c05af5e25e09ea33b45b946a7c581647e24df4
Jordy Verlaek <j.verlaek@student.fontys.nl>
2017-01-09 21:44:31 +0100

    Polished up some code for reacting

--- Plays.tv App/Database/IAccountContext.cs
+++ Plays.tv App/Database/IAccountContext.cs
@@ -8,6 +8,7 @@ namespace Plays.tv_App.Database
 {
     public interface IAccountContext
     {
+        //Accountcontext Interface
         Account Login(string username, string pw);
         List<Account> GetAll();
         bool Delete(int id);
